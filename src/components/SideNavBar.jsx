b007af0941b012cfd323a94a0704c1a068e73cd1
Daniel Nicholas <dansgoingcrazy@live.com>
2022-08-18 11:57:02 +1000

    imported an icon into react SideNavBar

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -1,8 +1,14 @@
+import { FaFolderOpen } from "react-icons/fa"
+import { IconContext } from "react-icons"
+
 const SideNavBar = () => {
   return (
+<IconContext.Provider value={{size: "3em"}}>
 <div className="SideNavBar">
   <h1>side nav bar</h1>
+  <FaFolderOpen />
 </div>
+</IconContext.Provider>
   )
 }
 export default SideNavBar
