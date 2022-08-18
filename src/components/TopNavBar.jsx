ade31b9496d47ec9481b47f9cb7bd94f5d5ae8dd
Daniel Nicholas <dansgoingcrazy@live.com>
2022-08-18 12:58:41 +1000

    more page styling

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,8 +1,10 @@
+import { FaUserAlt } from "react-icons/fa"
+
 const TopNavBar = () => {
   return (
 <div className = "TopNavBar">
 <h1>File Inspector</h1>
-<input type="text" placeholder="Search.."></input>
+<input type="text" placeholder="Search.."></input><div id="Icon-Top"><FaUserAlt size="2em"/></div>
 </div>
   )
 }
