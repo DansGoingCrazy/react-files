607ba93b45c0cd34a431e9dd6bbb581b04a9391b
Daniel Nicholas <dansgoingcrazy@live.com>
2022-08-18 12:37:49 +1000

    SideNavBar styling complete

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -1,14 +1,25 @@
 import { FaFolderOpen } from "react-icons/fa"
+import { MdHomeFilled } from "react-icons/md"
+import { IoIosSettings } from "react-icons/io"
 import { IconContext } from "react-icons"
 
 const SideNavBar = () => {
   return (
-<IconContext.Provider value={{size: "3em"}}>
+<IconContext.Provider value={{size: "2em"}} >
 <div className="SideNavBar">
-  <h1>side nav bar</h1>
+  <div id="Icon">
+  <MdHomeFilled />
+  </div>
+  <div id="Icon">
   <FaFolderOpen />
+  </div>
+  <div id="Icon">
+  <IoIosSettings />
+  </div>
+
 </div>
 </IconContext.Provider>
+
   )
 }
 export default SideNavBar
