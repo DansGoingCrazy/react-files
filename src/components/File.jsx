d777811bffe092d514d7e4dd1768093ae40215f8
Daniel Nicholas <dansgoingcrazy@live.com>
2022-08-23 09:38:13 +1000

    when clicking on a File in the List column, onclick is now console logging the FILEID

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,7 +1,7 @@
 const File = (props) => {
 // console.log(props)
   return (
-  <li className="file" id={props.file_number}>
+  <li className="file" id={props.id} onClick={() => console.log(props.id)}>
     <div className="Col1">
       <p>{props.number} - {props.title}</p>
       {/* <p>Orders: </p> */}
