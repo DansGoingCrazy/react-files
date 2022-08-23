0917b89e9d7dcc9e99316958bcccea48c03ba78e
Daniel Nicholas <dansgoingcrazy@live.com>
2022-08-23 14:52:22 +1000

    made columns scrollable and set their size to the vh minus the (heading height + 5rem)

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,10 +3,10 @@ const Application = (props) => {
   return (
     <li className='Application'>
       <h4>{props.description}</h4>
-      <p>Applicant: {props.applicant}</p> 
-      <p>Filing Date: {props.filed}</p> 
-      <p>Orders Sought: {props.ordersSought}</p> 
-      <p>Status: {props.orderStatus} - {props.orderStatusDate}</p> 
+      <p><b>Applicant:</b> {props.applicant}</p> 
+      <p><b>Filing Date:</b> {props.filed}</p> 
+      <p><b>Orders Sought:</b> {props.ordersSought}</p> 
+      <p><b>Status:</b> {props.orderStatus} - {props.orderStatusDate}</p> 
     </li>
   )
 }
